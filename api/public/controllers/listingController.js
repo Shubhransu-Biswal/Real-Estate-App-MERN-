42ffc759c708bd76231a5e4157ebe86acb7c2390
Shubhransu-Biswal <jaysriram8100@gmail.com>
2023-11-08 20:52:47 +0530

    created search api route

--- api/public/controllers/listingController.js
+++ api/public/controllers/listingController.js
@@ -63,7 +63,6 @@ export const updateListing = catchAsync(async (req, res, next) => {
 });
 
 // read
-
 export const getListing = catchAsync(async (req, res, next) => {
   // cheking if listing exists
   const list = await Listing.findById(req.params.id);
@@ -75,6 +74,58 @@ export const getListing = catchAsync(async (req, res, next) => {
     status: "success",
     body: {
       listing: list,
+    },
+  });
+});
+
+// read all
+export const getListings = catchAsync(async (req, res, next) => {
+  const limit = parseInt(req.query.limit) || 9;
+
+  const startIndex = parseInt(req.query.startIndex) || 0;
+
+  let offer = req.query.offer;
+  if (offer === undefined || offer === "false") {
+    offer = { $in: [false, true] };
+  }
+
+  let furnished = req.query.furnished;
+  if (furnished === undefined || furnished === "false") {
+    furnished = { $in: [false, true] };
+  }
+
+  let parking = req.query.parking;
+  if (parking === undefined || parking === "false") {
+    parking = { $in: [true, false] };
+  }
+
+  let type = req.query.type;
+  if (type === undefined || type === "all") {
+    type = { $in: ["sale", "rent"] };
+  }
+
+  const searchTerm = req.query.searchTerm || "";
+
+  const sort = req.query.sort || "createdAt";
+
+  const order = req.query.order || "desc";
+
+  const listings = await Listing.find({
+    // regex will match ther word and option will deactive the case sensitiveness
+    name: { $regex: searchTerm, $options: "i" },
+    offer,
+    furnished,
+    parking,
+    type,
+  })
+    .sort({ [sort]: order })
+    .limit(limit)
+    .skip(startIndex);
+
+  return res.status(200).json({
+    status: "success",
+    body: {
+      listings,
     },
   });
 });
--- api/public/routers/listingRouter.js
+++ api/public/routers/listingRouter.js
@@ -4,6 +4,7 @@ import {
   deleteListing,
   updateListing,
   getListing,
+  getListings,
 } from "../controllers/listingController.js";
 import { protect } from "../controllers/authController.js";
 const router = express.Router();
@@ -12,5 +13,6 @@ router.route("/create").post(protect, createListing);
 router.route("/delete/:id").delete(protect, deleteListing);
 router.route("/update/:id").patch(protect, updateListing);
 router.route("/read/:id").get(getListing);
+router.route("/read").get(getListings);
 
 export default router;
