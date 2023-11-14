a4115ba80fcb84bcf2cf040c8fe4fbdb2e4115cd
Shubhransu-Biswal <jaysriram8100@gmail.com>
2023-11-14 17:32:21 +0530

    changed listing bg color

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -50,7 +50,7 @@ const Listing = () => {
   }, [params.id]);
 
   return (
-    <main className="bg-[#151515ff]/50 overflow-hidden">
+    <main className="bg-[#151515ff]/10 overflow-hidden">
       {loading && <p className="text-center my-7 text-2xl">Loading...</p>}
       {error && (
         <p className="text-center my-7 text-2xl">Something went wrong!</p>
